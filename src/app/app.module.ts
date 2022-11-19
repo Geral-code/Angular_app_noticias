a14fa516c0063444ce434cae6866a416dcb96d39
Geraldine <geraldine.orellana@gmail.com>
2022-11-19 19:06:01 -0300

    crea navbar y jumbotron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormulariosComponent } from './components/formularios/formularios.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormulariosComponent,
+    ListadoNoticiasComponent,
+    NavbarComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule
